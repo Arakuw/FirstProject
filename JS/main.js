2bd1fbeb6408a70c9e26198affaecdf872b8d64d
Arailym <atemirbolatova@mail.ru>
2021-06-22 11:55:43 +0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+//console.log("JavaScript говорит привет");
+let person= {
+    Name:"John",
+    Age:20,
+    isMarried: false
+
+};
+console.log(person.Age);
+console.log(person["Age"]);
+
+let arr =['gf','dfsf','fgse'];
+console.log(arr[0]);
+
+//alert ("Hello world");
+//confirm("Are you here");
+
+let answer = prompt("Ecть ли вам 18?","");
+console.log(answer)
